35f973e45714ccb914687aed0fdb6b359d3e6395
peiran.xu <peiran.xu@ly.com>
2020-04-27 17:44:18 +0800

    add 电话号码的字母组合

---
+++ 17.电话号码的字母组合.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=17 lang=python3
+#
+# [17] 电话号码的字母组合
+#
+
+# @lc code=start
+
+
+class Solution:
+    def letterCombinations(self, digits):
+        conversion = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        if len(digits) == 0:
+            return []
+        product = ['']
+        for k in digits:
+            product = [i + j for i in product for j in conversion[k]]
+        return product
+# @lc code=end
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.letterCombinations('23'))
