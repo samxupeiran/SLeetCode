299f54e1175cd89c531e75e376aacaf257019008
peiran.xu <peiran.xu@ly.com>
2020-05-11 17:49:28 +0800

    update 电话号码的字母组合

--- 17.电话号码的字母组合.py
+++ 17.电话号码的字母组合.py
@@ -5,10 +5,11 @@
 #
 
 # @lc code=start
+from typing import List
 
 
 class Solution:
-    def letterCombinations(self, digits):
+    def letterCombinations(self, digits: str) -> List[str]:
         conversion = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         if len(digits) == 0:
             return []
